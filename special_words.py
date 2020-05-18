97cdb8cb8300521cbbfd66151b0fe6373e028d35
MHonauer <58992028+MHonauer@users.noreply.github.com>
2020-05-18 17:29:12 +0200

    Added many  faily words to 'special_words.py'

---
+++ special_words.py
@@ -0,0 +1,59 @@
+family = [
+
+    # a few different words for "mother" in German
+    "mutter",
+    "mutti",
+    "mama",
+    "mami",
+    "mum",
+    "mammi",
+    "mamma",
+    "mummy",
+    "stiefmama",
+    "stiefmami",
+    "stiefmutter",
+    
+    # a few different words for "father" in German 
+    "vater",
+    "vati",
+    "papa",
+    "papi",
+    "dad",
+    "daddy",
+    "paps",
+    "stiefpapa",
+    "stiefpapi",
+    "stiefvater",
+
+
+    # a few different words for "brother" in German
+    "bruder",
+    "brüderchen",
+    "bruderherz",
+
+    # a few different words for "sister" in German
+    "schwester",
+    "schwesterchen",
+    "schwesterherz",
+
+    # Words for "grandfather" and "grandmother" in German
+    "opa",
+    "oma",
+    "opi",
+    "omi",
+    "großvater",
+    "großmutter",
+    "großpapa",
+    "großmama",
+    "großpapi",
+    "großmami",
+
+    # Another family words in German
+    "zwilling",
+    "zwillinge",
+
+    "drilling",
+    "drillinge",
+    "vierling",
+    "vierlinge",
+] 
