b65728f90caf13ad6459a81d106d6a4be48fdc92
MHonauer <58992028+MHonauer@users.noreply.github.com>
2020-05-19 13:31:01 +0200

    Added function "read_input()" to functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,20 @@
 # Functon definitions for EMMA
+
+def read_input():
+
+    # Input text
+    inp = input()
+
+    # Remove spaces at the beginning and the end of the input
+    inp = inp.strip()
+
+    # Combine several spaces in a row into one
+    while not inp.find("  ") < 0:
+        inp = inp.replace("  ", " ")
+
+    # Remove the dot at the end
+    if inp.endswith("."):
+        inp = inp[:-1]
+
+    # Convert everything to lowercase
+    inp = inp.lower()
