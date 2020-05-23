55cc4514b6a7e7c5d4ebcfc0fa5ec2234419e28f
MHonauer <58992028+MHonauer@users.noreply.github.com>
2020-05-23 20:07:23 +0200

    Added jokes.yp for jokes and poems.py for poems and filled them

---
+++ jokes.py
@@ -0,0 +1,12 @@
+# Contains jokes for telling them
+jokes = [
+    "Was sagt ein Haifisch, wenn er einen Surfer sieht?\n'Das ist aber nett serviert, so mit Frühstücksbrettchen.'",
+    "Geht eine schwangere Frau in eine Bäckerei und sagt: 'Ich krieg ein Brot.'\nDarauf der Bäcker: 'Sachen gibt's!'",
+    "Geht ein Fahrkartenautomat zum Arzt: 'Herr Doktor, ich krieg die ganze Zeit Geldschein eingesteckt aber spucke nur Münzen aus.\nWas ist nur los mit mir?'\nDoktor: 'Sie sind in den Wechseljahren.'",
+    "Was ist braun, knusprig und schwimmt Unterwasser?\nEin Ubrot",
+    "Klingelt man beim Metzger, dann sollte man sich nicht wundern, dass kein Schwein aufmacht!",
+    "Was liegt auf dem Grund des Ozeans und zittert?\nEin nervöses Wrack",
+    "Ich habe heute Morgen meinen Chef gefragt, ob ich auch ein bisschen später zur Arbeit kommen darf.\nEr meinte 'Träum weiter'. Voll nett vom ihm, oder?",
+    "Sitzen zwei Hochhäuser auf dem Dachboden und stricken Benzin.\nWas stimmt nicht?\nBenzin darf man nur Häkeln!",
+    "Willst du einen Baustellenwitz hören?\nJa\nOk, ich arbeite daran."
+]
