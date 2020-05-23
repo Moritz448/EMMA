1e25109c7dd873d9fe087e7c82fb74931d784e6d
MHonauer <58992028+MHonauer@users.noreply.github.com>
2020-05-23 18:22:38 +0200

    Added a better KeyboardInterrupt message

--- main.py
+++ main.py
@@ -6,39 +6,43 @@ import random
 
 def emma():
 
-    print("\n##############################################################\nHallo! Mein Name ist EMMA. Schön, dass du",
-          "mit mir\nsprechen willst. Wenn du keine Lust",
-          "mehr hast, sag\neinfach 'Tschüss'.",
-          "Was möchtest du mir heute erzählen?",
-          "\n##############################################################\n")
+    try:
+
+        print("\n##############################################################\nHallo! Mein Name ist EMMA. Schön, dass du",
+              "mit mir\nsprechen willst. Wenn du keine Lust",
+              "mehr hast, sag\neinfach 'Tschüss'.",
+              "Was möchtest du mir heute erzählen?",
+              "\n##############################################################\n")
 
 
-    talk_ended = False
+        talk_ended = False
 
-    while not talk_ended:
-        inp = read_sentence()
+        while not talk_ended:
+            inp = read_sentence()
 
-        answered = False
+            answered = False
 
-        if inp == "tschüss" or inp == "tschüß":
-            print("Schade, dass du schon gehen willst. Bis bald!")
-            answered = True
-            talk_ended = True
+            if inp == "tschüss" or inp == "tschüß":
+                print("Schade, dass du schon gehen willst. Bis bald!")
+                answered = True
+                talk_ended = True
 
-        if not answered:
-            answered = i_am_sad(inp)
+            if not answered:
+                answered = i_am_sad(inp)
 
-        if not answered:
-            answered = reformulate_sentence(inp)
+            if not answered:
+                answered = reformulate_sentence(inp)
 
-        if not answered:
-            answered = ask_after_family(inp)
+            if not answered:
+                answered = ask_after_family(inp)
 
-        if not answered:
-            print(random.choice(comments), 
-                  random.choice(questions))
+            if not answered:
+                print(random.choice(comments), 
+                      random.choice(questions))
 
-        print()
+            print()
+    except KeyboardInterrupt:
+        print("Schade, dass du schon gehen willst. Bis bald!")
 
 
 emma()
