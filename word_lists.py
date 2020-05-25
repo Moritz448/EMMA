35613a67562da8c0cc85031fd39a69b5d5fd2c54
MHonauer <58992028+MHonauer@users.noreply.github.com>
2020-05-25 15:59:33 +0200

    Added more comments and questions to word_lists.py

--- word_lists.py
+++ word_lists.py
@@ -1,12 +1,15 @@
 comments = ["Das klingt interessant.",
             "Aha.",
             "Interessant.",
-            "Klingt gut."]
+            "Klingt gut.",
+            "Klingt interessant"]
 
 questions = ["Erzähl mir mehr davon!",
             "Worüber willst du noch sprechen?",
             "Und weiter?",
-            "Was möchtest du mir noch sagen?"]
+            "Was möchtest du mir noch sagen?",
+            "Was gibt es noch zu sagen?",
+            "Möchtest du mir noch etwas erzählen?"]
 
 
 # Contains several variations of "no" in German
