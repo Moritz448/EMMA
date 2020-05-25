bc56d994f030e09b5bddef7e2bb9748d6029fb68
MHonauer <58992028+MHonauer@users.noreply.github.com>
2020-05-25 15:00:48 +0200

    Changed the import function in main.py to be more speciefic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from functions import *
+from functions import reformulate_sentence, i_am_sad, tell_me_poem, is_insulting, reformulate_sentence, ask_after_family, read_sentence
 from word_lists import comments, questions
 
 import random
@@ -32,9 +32,6 @@ def emma():
 
             if not answered:
                 answered = tell_me_poem(inp)
-
-            if not answered:
-                answered = tell_me_joke(inp)
 
             if not answered:
                 answered = is_insulting(inp)
